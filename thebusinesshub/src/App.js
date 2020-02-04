55cd33b94632b91d5dc20e709be593a2950dfba7
Radwa119 <radwaragab119@gmail.com>
2020-02-04 20:26:53 +0200

    added login and register forms, packages and setup components

--- thebusinesshub/src/App.js
+++ thebusinesshub/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+// Libraries
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignInForm from '../src/components/forms/SignInForm'
+// Styling
 import './App.css';
+import SignInUp from './components/forms/SignInUp';
+// Components
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <SignInUp />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
