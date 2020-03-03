a5c81005906760ae853242849c04f72172c75328
= <nourhassan132@gmail.com>
2020-03-03 04:32:21 -0800

    cards

--- thebusinesshub/src/components/user/purchasee/Purchasecard.js
+++ thebusinesshub/src/components/user/purchasee/Purchasecard.js
@@ -1,69 +1,164 @@
 import React, { Component } from 'react';
-import { Container , Card , Button ,FormControl ,Row , Col} from 'react-bootstrap';
+import { Container , Card , Button  ,Row , Col,Form} from 'react-bootstrap';
 import '../../stylesheets/purchasecardCSS.css';
 export default class Purchasecard extends Component {
 constructor(props){
   super(props);
   this.state={
-    hrsNumber:'',
-    cards:[{img:'../../../Images/img.png' , HRS:'+10'},
-    {img:'../../../Images/bg.png', HRS:'+50'},
-    {img:'../../../Images/img.png' ,HRS:'+100'} ],
+ 
+    tenhoursvalue:'',
+    thirtyvalue:'',
+    fiftyvalue:'',
     showPurchasepopup:false
   }
 }
 
-  sethoursNumber = e => {
-    this.setState({ hrsNumber: e.target.value });
+  settenhours = e => {
+     this.setState({tenhoursvalue:e.target.value})
   };
+
+  setthirtyhours = e => {
+    this.setState({thirtyvalue:e.target.value})
+ };
+
+ setfiftyhours = e => {
+  this.setState({fiftyvalue:e.target.value})
+};
+
+
+ 
 
   showPurchasepopup(){
     this.setState({showPurchasepopup:true})
   }
+
   render() {
     
+
     return (
       
       <div>
       <Container>
       <Row>
 
-          {this.state.cards.map((card,i)=>(
+         
              <Col sm={12} md={4}>
           <Card style={{ width: '18rem' }}>
       
       <div className="cardimg">
      
 
-         <Card.Img  src={card.img} />
-      {card.HRS}
+         <Card.Img  src={require('../../../Images/img.png')} />
+     
          </div>
   <Card.Body>
     <Card.Text>
     
-                      <div className="monthdrop">
-                        <FormControl
-                          style={{ height: '20px', margin: '0px' , width:'50%' }}
-                          className="startTime"
-                          type="number"
-                          name="hrsNumber"
-                          value={this.state.hrsNumber}
-                          onChange={this.sethoursNumber}
-                          min="1"
-                          max="5"
-                        /> 
+                <div className="monthdrop">
+                <Form.Control
+               style={{ height: '20px', margin: '0px' , width:'50%' }}
+               className="startTime"
+              type="text"
+              value={this.state.tenhoursvalue}
+              onChange={this.settenhours}
+              name='tenhours'
+              
+            />
+         
+     
+
                       </div>
     </Card.Text>
   </Card.Body>
  
   <Card.Footer>
     <div className="cardbtn"> 
-    <Button className="Purchasecard" onClick={this.showPurchasepopup}>Purchase</Button></div>
+    <Button className="Purchasecard"
+     onClick={this.showPurchasepopup}>Purchase</Button></div>
     
   </Card.Footer>
 </Card>     
 </Col>
-       )) }
+   
+
+
+<Col sm={12} md={4}>
+          <Card style={{ width: '18rem' }}>
+      
+      <div className="cardimg">
+     
+
+         <Card.Img  src={require('../../../Images/img.png')} />
+     
+         </div>
+  <Card.Body>
+    <Card.Text>
+    
+                <div className="monthdrop">
+                <Form.Control
+               style={{ height: '20px', margin: '0px' , width:'50%' }}
+               className="startTime"
+              type="text"
+              value={this.state.thirtyvalue}
+              onChange={this.setthirtyhours}
+              name='thirtyhours'
+              
+            />
+         
+     
+
+                      </div>
+    </Card.Text>
+  </Card.Body>
+ 
+  <Card.Footer>
+    <div className="cardbtn"> 
+    <Button className="Purchasecard"
+     onClick={this.showPurchasepopup}>Purchase</Button></div>
+    
+  </Card.Footer>
+</Card>     
+</Col>
+
+
+
+<Col sm={12} md={4}>
+          <Card style={{ width: '18rem' }}>
+      
+      <div className="cardimg">
+     
+
+         <Card.Img  src={require('../../../Images/img.png')} />
+     
+         </div>
+  <Card.Body>
+    <Card.Text>
+    
+                <div className="monthdrop">
+                <Form.Control
+               style={{ height: '20px', margin: '0px' , width:'50%' }}
+               className="startTime"
+              type="text"
+              value={this.state.fiftyvalue}
+              onChange={this.setfiftyhours}
+              name='fiftyhours'
+              
+            />
+         
+     
+
+                      </div>
+    </Card.Text>
+  </Card.Body>
+ 
+  <Card.Footer>
+    <div className="cardbtn"> 
+    <Button className="Purchasecard"
+     onClick={this.showPurchasepopup}>Purchase</Button></div>
+    
+  </Card.Footer>
+</Card>     
+</Col>
          
         
          </Row> 
